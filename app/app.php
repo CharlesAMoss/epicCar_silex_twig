5f4496253b1613a2ae5f63defc78addca7865e90
Kenny Grage & charlie <student@epicodus.com>
2015-08-06 12:11:55 -0700

    creates twig templetes

--- app/app.php
+++ app/app.php
@@ -3,80 +3,52 @@
   require_once __DIR__."/../src/car.php";
 
   $app = new Silex\Application();
-  
-  $app->get("/", function() {
-    return "
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-      <title>Search for a car!</title>
-    </head>
+  $app['debug'] = true;
+  $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+  ));
 
-    <body>
-      <div class='container'>
-        <h1>Car Search</h1>
-        <p>Enter the price and mileage in a car that you are looking for.</p>
-        <form action='/search_results'>
-          <div class='form-group'>
-            <label for='user_price'>Enter a price:</label>
-            <input id='user_price' name='user_price' class='form-control' type='number'>
-          </div>
-          <div class='form-group'>
-            <label for='user_miles'>Enter a mileage:</label>
-            <input id='user_miles' name='user_miles' class='form-control' type='number'>
-          </div>
-          <button type='submit' class='btn-success'>Search</button>
-        </form>
-    </body>
-    </html>
-    ";
-    });
+  $app->get("/", function() use ($app) {
 
-    $app->get("/search_results", function() {
+    return $app['twig']->render('dealership.html.twig');
 
-      $porsche = new Car("2014 Porsche 911", 114991, 7864, "/img/porsche.jpg");
-      $ford = new Car("2011 Ford F450", 55995, 14241, "/img/ford.jpg");
-      $lexus = new Car("2013 Lexus RX 350", 44700, 20000, "/img/lexus.jpg");
-      $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "/img/cls550.jpg");
-      $mercedes->setPrice(35000.925);
-      $porsche->setPrice("hotdog");
-      $user_price = $_GET["user_price"];
-      $user_miles = $_GET["user_miles"];
+  });
 
-      $cars = array($porsche, $ford, $lexus, $mercedes);
-      $arrayOfCars = searchCars($cars, $user_price, $user_miles);
-      $whatToReturn = "";
-      if (empty($arrayOfCars)) {
-        $whatToReturn = '<p>No cars match your search</p>';
+  $app->get("/search_results", function() use ($app) {
+
+    $porsche = new Car("2014 Porsche 911", 114991, 7864, "/img/porsche.jpg");
+    $ford = new Car("2011 Ford F450", 55995, 14241, "/img/ford.jpg");
+    $lexus = new Car("2013 Lexus RX 350", 44700, 20000, "/img/lexus.jpg");
+    $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "/img/cls550.jpg");
+    $mercedes->setPrice(35000.925);
+    $porsche->setPrice("hotdog");
+    $user_price = $_GET["user_price"];
+    $user_miles = $_GET["user_miles"];
+
+    $cars = array($porsche, $ford, $lexus, $mercedes);
+    $arrayOfCars = searchCars($cars, $user_price, $user_miles);
+    $whatToReturn = "";
+    if (empty($arrayOfCars)) {
+      $whatToReturn = '<p>No cars match your search</p>';
+    }
+    else {
+      foreach ($arrayOfCars as $specific_car) {
+        $car_price = $specific_car->getPrice();
+        $car_make = $specific_car->getMake();
+        $car_miles = $specific_car->getMiles();
+        $car_picture = $specific_car->getPicture();
+        $whatToReturn .= "<div><img src='$car_picture'</div>
+              <p>$car_make</p>
+              <p>$car_miles miles</p>
+              <p>$$car_price</p>
+            ";
       }
-      else {
-        foreach ($arrayOfCars as $specific_car) {
-          $car_price = $specific_car->getPrice();
-          $car_make = $specific_car->getMake();
-          $car_miles = $specific_car->getMiles();
-          $car_picture = $specific_car->getPicture();
-          $whatToReturn .= "<div><img src='$car_picture'</div>
-                <p>$car_make</p>
-                <p>$car_miles miles</p>
-                <p>$$car_price</p>
-              ";
-        }
-      }
+    }
 
-      return "
-        <!DOCTYPE html>
-        <html>
-        <head>
-        </head>
-        <body>
-          <div class='container'>
-            $whatToReturn
-          </div>
-        </body>
-        </html>
-      ";
-    });
+    // creates the array with one index: the key value of the String $whatToReturn 
+    return $app['twig']->render('search_results.html.twig', array('whatToReturn' => $whatToReturn));
+
+  });
 
     return $app;
 ?>
